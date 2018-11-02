3c1e69687487fe07e3c2f63b545fbf7e96a72830
Silesh K Nair <sileshn@gmail.com>
2018-11-03 05:01:21 +0530

    Revert "msm8930-common: Move wcnss_service to vendor"
    This reverts commit 9a18a77816fa6a6f7dd6e2d2562fb84fa696e676.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,9 +80,6 @@ WIFI_DRIVER_MODULE_NAME          := "wlan"
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 
-# Move wcnss_service to vendor
-PRODUCT_VENDOR_MOVE_ENABLED := true
-
 # DexPreopt debug info
 WITH_DEXPREOPT_DEBUG_INFO := false
 
